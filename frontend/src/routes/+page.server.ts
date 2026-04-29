2eb1753019b6830b593ab219e1fc661ea92d5118
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-04-29 09:21:36 -0300

    frontend com svelte criado

---
+++ frontend/src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+export const load = async ({ locals }) => {
+	return {
+		user: locals.user
+	};
+};
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+    import { type PageData } from "$lib/types";
+
+	const { data }: {data: PageData} = $props();
+</script>
+
+{#if data.user}
+	<Button>{data.user.name}</Button>
+{/if}
